47850df3a01a4f9899112f882d04d60f51aca4f3
Erica Ford <erica.ford0205@gmail.com>
2023-06-28 10:31:34 -0500

    route pages using Outlet and RouterProvider in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
 import "./App.css";
+import { Fragment } from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Post from "./pages/Post";
+import Write from "./pages/Write";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 function App() {
-  return <div className="App">App</div>;
+  const Layout = () => {
+    return (
+      <Fragment>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </Fragment>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/post/:id",
+          element: <Post />,
+        },
+        {
+          path: "/write",
+          element: <Write />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
