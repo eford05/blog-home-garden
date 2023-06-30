7106eca6e236d91903c3e9d29da395299f7acce2
Erica Ford <erica.ford0205@gmail.com>
2023-06-30 10:07:01 -0500

    add data file for handling posts

---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+export const posts = [
+  {
+    id: 1,
+    src: "",
+    title: "",
+    desc: "",
+    auth: "",
+    post: "",
+    cat: "",
+    main: true,
+    trending: false,
+    featured: false,
+  },
+];
