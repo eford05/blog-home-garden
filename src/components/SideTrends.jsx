cca6e8db6952eb376b3e838b65c112109f4e2938
Erica Ford <erica.ford0205@gmail.com>
2023-07-05 13:39:57 -0500

    update currentPost onClick side trends

--- src/components/SideTrends.jsx
+++ src/components/SideTrends.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import { useContext } from "react";
 import { posts } from "../data/data";
 import { Link } from "react-router-dom";
+import { PostContext } from "../contexts/PostContext";
 import "../styles/sidetrends.css";
 
+//Update current post with post id using context
 function SideTrends() {
+  const { setCurrentPost } = useContext(PostContext);
   return (
     <section className="side-trends">
       <h2 className="side-trends-title">Trending</h2>
@@ -14,7 +17,7 @@ function SideTrends() {
             <figure className="trend-card-fig">
               <img src={post.src} alt={post.alt} />
             </figure>
-            <Link to={post.path}>
+            <Link to={post.path} onClick={() => setCurrentPost(post.id)}>
               <h3 className="trend-card-title">{post.title}</h3>
             </Link>
             <p className="trend-card-author">{post.auth}</p>
